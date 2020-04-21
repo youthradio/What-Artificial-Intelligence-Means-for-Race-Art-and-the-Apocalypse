3034a512d125e5522b8828b1dc09c4c7400a5b32
Radames Ajna <radamajna@gmail.com>
2020-04-20 17:53:02 -0700

    new Title, copy edits

--- post.config.js
+++ post.config.js
@@ -1,18 +1,18 @@
 export default {
-  baseURL: '/the-dread-play-artificial-Intelligence',
-  title: 'Investigating the Dread + Play of Artificial Intelligence',
+  baseURL: '/What-Artificial-Intelligence-Means-for-Race-Art-and-Apocalypse',
+  title: 'In the Black Mirror What Artificial Intelligence Means for Race, Art and Apocalypse',
   author: 'Static Author',
   publishDate: 'April. 14, 2020',
   location: 'Oakland, CA',
   description: 'SEO friendly Description',
-  url: 'https://interactive.yr.media/the-dread-play-artificial-Intelligence',
+  url: 'https://interactive.yr.media/What-Artificial-Intelligence-Means-for-Race-Art-and-Apocalypse',
   featureImage:
-    'https://interactive.yr.media/the-dread-play-artificial-Intelligence/images/feature_image.jpg',
+    'https://interactive.yr.media/What-Artificial-Intelligence-Means-for-Race-Art-and-Apocalypse/images/feature_image.jpg',
   featureImagePath: 'images/feature',
   featureImageDescription: 'Woman looking at kiosk.',
   featureImageCaption: '(Photo: Andersen Ross Photography Inc/Getty Images)',
 
-  wpPostSlug: 'north-carolina-vs-vaping-companies',
+  wpPostSlug: 'Black-Mirror-What-Artificial-Intelligence-Means-for-Race-Art-and-Apocalypse',
   wpPostID: '60986',
   // you might not need to change
   fbAppID: '73080818131',
